0de9f05a8c652898890765c640114daa5c52cfab
Alex Duan <alexduan@gmail.com>
2016-08-26 16:53:32 -0400

    added search widget

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,15 @@
 class DashboardController < ApplicationController
 
   def index
+    query = params[:q]
+
     @date = Date.today
 
+    if query.nil?
+      @employees = Employee.all
+    else
+      @employees = Employee.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{query}%", "%#{query}%")
+    end
 
   end
 end
