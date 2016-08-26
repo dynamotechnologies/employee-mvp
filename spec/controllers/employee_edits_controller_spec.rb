17ebbff95692e2d20bdb68df7aa3e45c878d53dd
csterritt <csterritt@gmail.com>
2016-08-26 19:32:26 -0400

    Added EmployeeEdit tests to show only admins can edit.

--- spec/controllers/employee_edits_controller_spec.rb
+++ spec/controllers/employee_edits_controller_spec.rb
@@ -20,139 +20,33 @@ require 'rails_helper'
 
 RSpec.describe EmployeeEditsController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # EmployeeEdit. As you add validations to EmployeeEdit, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # EmployeeEditsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  before :each do
+    @regular_user = FactoryGirl.create(:employee,
+                                       username: "jdoe", email: "jdoe1@jdoe.jdoe",
+                                       password: "secret", password_confirmation: "secret",
+                                       first_name: "Joe1", last_name: "Doe", kudos_received: 0, is_admin: false)
+    @admin_user = FactoryGirl.create(:employee,
+                                     username: "jdoe_admin", email: "jdoe@jdoe.jdoe",
+                                     password: "secret", password_confirmation: "secret",
+                                     first_name: "Joe", last_name: "Doe", kudos_received: 0, is_admin: true)
+  end
 
   describe "GET #index" do
-    it "assigns all employee_edits as @employee_edits" do
-      employee_edit = EmployeeEdit.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(assigns(:employee_edits)).to eq([employee_edit])
-    end
-  end
-
-  describe "GET #show" do
-    it "assigns the requested employee_edit as @employee_edit" do
-      employee_edit = EmployeeEdit.create! valid_attributes
-      get :show, params: {id: employee_edit.to_param}, session: valid_session
-      expect(assigns(:employee_edit)).to eq(employee_edit)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new employee_edit as @employee_edit" do
-      get :new, params: {}, session: valid_session
-      expect(assigns(:employee_edit)).to be_a_new(EmployeeEdit)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested employee_edit as @employee_edit" do
-      employee_edit = EmployeeEdit.create! valid_attributes
-      get :edit, params: {id: employee_edit.to_param}, session: valid_session
-      expect(assigns(:employee_edit)).to eq(employee_edit)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new EmployeeEdit" do
-        expect {
-          post :create, params: {employee_edit: valid_attributes}, session: valid_session
-        }.to change(EmployeeEdit, :count).by(1)
-      end
-
-      it "assigns a newly created employee_edit as @employee_edit" do
-        post :create, params: {employee_edit: valid_attributes}, session: valid_session
-        expect(assigns(:employee_edit)).to be_a(EmployeeEdit)
-        expect(assigns(:employee_edit)).to be_persisted
-      end
-
-      it "redirects to the created employee_edit" do
-        post :create, params: {employee_edit: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(EmployeeEdit.last)
-      end
+    it "allows an admin user" do
+      sign_in @admin_user
+      get :index
+      expect(response.code).to eq("200")
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved employee_edit as @employee_edit" do
-        post :create, params: {employee_edit: invalid_attributes}, session: valid_session
-        expect(assigns(:employee_edit)).to be_a_new(EmployeeEdit)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, params: {employee_edit: invalid_attributes}, session: valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested employee_edit" do
-        employee_edit = EmployeeEdit.create! valid_attributes
-        put :update, params: {id: employee_edit.to_param, employee_edit: new_attributes}, session: valid_session
-        employee_edit.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested employee_edit as @employee_edit" do
-        employee_edit = EmployeeEdit.create! valid_attributes
-        put :update, params: {id: employee_edit.to_param, employee_edit: valid_attributes}, session: valid_session
-        expect(assigns(:employee_edit)).to eq(employee_edit)
-      end
-
-      it "redirects to the employee_edit" do
-        employee_edit = EmployeeEdit.create! valid_attributes
-        put :update, params: {id: employee_edit.to_param, employee_edit: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(employee_edit)
-      end
+    it "disallows a regular user" do
+      sign_in @regular_user
+      get :index
+      expect(response.code).to eq("302")
     end
 
-    context "with invalid params" do
-      it "assigns the employee_edit as @employee_edit" do
-        employee_edit = EmployeeEdit.create! valid_attributes
-        put :update, params: {id: employee_edit.to_param, employee_edit: invalid_attributes}, session: valid_session
-        expect(assigns(:employee_edit)).to eq(employee_edit)
-      end
-
-      it "re-renders the 'edit' template" do
-        employee_edit = EmployeeEdit.create! valid_attributes
-        put :update, params: {id: employee_edit.to_param, employee_edit: invalid_attributes}, session: valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested employee_edit" do
-      employee_edit = EmployeeEdit.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: employee_edit.to_param}, session: valid_session
-      }.to change(EmployeeEdit, :count).by(-1)
-    end
-
-    it "redirects to the employee_edits list" do
-      employee_edit = EmployeeEdit.create! valid_attributes
-      delete :destroy, params: {id: employee_edit.to_param}, session: valid_session
-      expect(response).to redirect_to(employee_edits_url)
+    it "disallows when no user is logged in" do
+      get :index
+      expect(response.code).to eq("302")
     end
   end
 
