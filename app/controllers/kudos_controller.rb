1f840254a7ac12695ee883b40798b1f0b1d25e39
Alex Duan <alexduan@gmail.com>
2016-08-26 19:56:29 -0400

    begin constraint checks...

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -4,12 +4,20 @@ class KudosController < ApplicationController
   def give
     to = Employee.find(params[:to])
     amount = params[:amount].to_f
-    give_kudos(current_employee, to, amount)
+
+    begin
+      give_kudos(current_employee, to, amount)
+      response = {balance: current_employee.kudo_balance, to: to}
+      status = 200
+    rescue
+      response = {message: 'Balance too low'}
+      status = 412
+    end
 
 
     respond_to do |format|
       format.json {
-        render json: {balance: current_employee.kudo_balance, to: to}
+        render json: response, status: status
       }
     end
 
--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,6 +1,10 @@
 module KudosHelper
 
   def give_kudos(from, to, kudos)
+    if from.kudo_balance < kudos
+      raise 'User does not have enough kudos to give.'
+    end
+
     Employee.transaction do
       from.kudo_balance -= kudos
       from.save()
