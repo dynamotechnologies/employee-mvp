712869c2cefacc09140982728c8b21c1fd2c3c2d
Alex Duan <alexduan@gmail.com>
2016-08-26 16:10:19 -0400

    added default login to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+    :employee
+  end
+
+  def resource
+    @employee ||= Employee.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:employee]
+  end
 end
