e5f0602f66bb44fac8d152f8f50a621e5fc5669d
Jonathan Keam <jpkeam@gmail.com>
2016-08-27 03:11:39 -0400

    order the employees by kudos desc

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,12 @@ class DashboardController < ApplicationController
 
   def index
     query = params[:q]
-
     @date = Date.today
 
     if query.nil?
-      @employees = Employee.active
+      @employees = Employee.active.order(kudos_received: :desc)
     else
-      @employees = Employee.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{query}%", "%#{query}%").active
+      @employees = Employee.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{query}%", "%#{query}%").order(kudos_received: :desc).active
     end
-
   end
 end
