afa7a0584feaf152d4f10fd640e0e00659dd2a49
Alex Duan <alexduan@gmail.com>
2016-08-26 20:19:28 -0400

    default sign in

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'leaderboard' => 'leaderboard#index', as: 'leaderboard'
 
-  root to: "home#index"
+  root to: 'dashboard#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
