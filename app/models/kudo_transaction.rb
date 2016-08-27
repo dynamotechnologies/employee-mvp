14541a41e13c6d78b88a8cb3b3571ad1f64e7390
csterritt <csterritt@gmail.com>
2016-08-27 15:54:13 -0400

    Fix a bug due to from_user_must_have_enough_kudos runs after deduction.

--- app/models/kudo_transaction.rb
+++ app/models/kudo_transaction.rb
@@ -24,9 +24,9 @@ class KudoTransaction < ApplicationRecord
       .order("month_total desc").limit(limit)
   end
 
-  private 
+  private
     def from_user_must_have_enough_kudos
       e = Employee.find self.from_id
-      errors.add(:amount, "is more than kudos remaining") if e.kudo_balance < self.amount
+      errors.add(:amount, "is more than kudos remaining") if e.kudo_balance < 0
     end
 end
