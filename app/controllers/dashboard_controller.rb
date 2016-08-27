7af6e35bea5cdd543724cb8ea1fca3b5c7e9fc8c
Alex Duan <alexduan@gmail.com>
2016-08-27 13:44:30 -0400

    default dashboard no search results

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,8 @@ class DashboardController < ApplicationController
     query = params[:q]
     @date = Date.today
 
-    if query.nil?
-      @employees = Employee.active.order(kudos_received: :desc)
-    else
+    @employees = []
+    if !query.nil?
       @employees = Employee.where('first_name ILIKE ? OR last_name ILIKE ?', "%#{query}%", "%#{query}%").order(kudos_received: :desc).active
     end
   end
