25182c195f41ac59ccdf42303f07586b6e2d74ac
Jonathan Keam <jpkeam@gmail.com>
2016-08-26 23:08:29 -0400

    added spec for kudos transfer

--- spec/controllers/kudos_controller_spec.rb
+++ spec/controllers/kudos_controller_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe KudosController, type: :controller do
+  render_views
+
+  before :each do
+     @regular_user = FactoryGirl.create(:employee,
+                                       username: "jdoe", email: "jdoe1@jdoe.jdoe",
+                                       password: "secret", password_confirmation: "secret",
+                                       first_name: "Joe1", last_name: "Doe", kudo_balance: 100, is_admin: false)
+
+    @admin_user = FactoryGirl.create(:employee,
+                                     username: "jdoe_admin", email: "jdoe@jdoe.jdoe",
+                                     password: "secret", password_confirmation: "secret",
+                                     first_name: "Joe", last_name: "Doe", kudo_balance: 100, is_admin: true)
+  end
+
+  describe "POST #kudos" do
+    it "allows kudo transfer" do
+      sign_in @admin_user
+      post :give, params: {kudo_transaction: {to_id: @regular_user.id, amount: 5}, format: :json}
+
+      resp = JSON.parse(response.body)
+      expect(response.code).to eq("200")
+      expect(resp['from_id']).to eq(@admin_user.id)
+      expect(resp['to_id']).to eq(@regular_user.id)
+      expect(resp['amount']).to eq(5)
+    end
+
+    it "should not allow kudo transfer if not enough balance" do
+      sign_in @admin_user
+      post :give, params: {kudo_transaction: {to_id: @regular_user.id, amount: 500}, format: :json}
+
+      resp = JSON.parse(response.body)
+      expect(response.code).to eq("422")
+      expect(resp['amount']).to eq(["is more than kudos remaining"])
+    end
+  end
 
 end
