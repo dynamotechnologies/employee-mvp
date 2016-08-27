00432bccea2be4844b88b33943faa26cf06b15df
Alex Duan <alexduan@gmail.com>
2016-08-27 16:06:29 -0400

    updated leaderboard to use monthly ranking, use proper scope

--- app/models/monthly_ranking.rb
+++ app/models/monthly_ranking.rb
@@ -2,4 +2,6 @@ class MonthlyRanking < ApplicationRecord
   validates :employee_id, presence: true
 
   belongs_to :employee, class_name: :Employee, foreign_key: "employee_id"
+
+  scope :by_month, -> (month = Date.today.month, year = Date.today.year) {where('month = ? and year = ?', month, year)}
 end
