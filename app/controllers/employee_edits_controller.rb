e964dc71b25f69e82a66d27724e838e288a158a6
csterritt <csterritt@gmail.com>
2016-08-29 13:03:42 -0400

    Make employees active/inactive

--- app/controllers/employee_edits_controller.rb
+++ app/controllers/employee_edits_controller.rb
@@ -10,8 +10,8 @@ class EmployeeEditsController < ApplicationController
 
     @employees = Employee.none.page(params[:page])
     unless @search_term.blank?
-      @employees = Employee.active.where("first_name ILIKE ? OR last_name ILIKE ?", "%#{@search_term}%", "%#{@search_term}%").
-          order(last_name: :asc, first_name: :asc).active.page(params[:page])
+      @employees = Employee.where("first_name ILIKE ? OR last_name ILIKE ?", "%#{@search_term}%", "%#{@search_term}%").
+          order(last_name: :asc, first_name: :asc).page(params[:page])
     end
   end
 
@@ -52,9 +52,9 @@ class EmployeeEditsController < ApplicationController
       params[:employee].delete(:password)
       params[:employee].delete(:password_confirmation)
     end
-    if params[:employee][:is_admin].blank?
-      params[:employee][:is_admin] = false
-    end
+
+    params[:employee][:is_admin] = false if params[:employee][:is_admin].blank?
+    params[:employee][:is_active] = false if params[:employee][:is_active].blank?
 
     respond_to do |format|
       if @employee.update(employee_params)
@@ -87,6 +87,6 @@ class EmployeeEditsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def employee_params
     #params.fetch(:employee, {})
-    params.require(:employee).permit(:first_name, :email, :last_name, :password, :password_confirmation, :is_admin)
+    params.require(:employee).permit(:first_name, :email, :last_name, :password, :password_confirmation, :is_admin, :is_active)
   end
 end
