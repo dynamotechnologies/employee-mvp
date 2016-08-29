c296230dd4a839b50f64abc86d6ef070e278dcfa
csterritt <csterritt@gmail.com>
2016-08-29 11:46:41 -0400

    Employee model update, new seeds.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827191017) do
+ActiveRecord::Schema.define(version: 20160829153121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20160827191017) do
     t.string   "last_name",              default: "",    null: false
     t.boolean  "is_admin",               default: false, null: false
     t.boolean  "is_active",              default: true,  null: false
-    t.integer  "kudo_balance",           default: 0,     null: false
-    t.integer  "kudos_received",         default: 0,     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -35,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160827191017) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.text     "middle_initial"
     t.index ["email"], name: "index_employees_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_employees_on_username", unique: true, using: :btree
